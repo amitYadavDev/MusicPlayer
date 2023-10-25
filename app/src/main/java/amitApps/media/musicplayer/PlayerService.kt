323d8aa5fd12a7bbe812f927033ed511199421de
amitYadavDev <amit8955461017@gmail.com>
2023-10-25 17:35:58 +0530

    Small and Large Notification are getting coded.

--- app/src/main/java/amitApps/media/musicplayer/PlayerService.kt
+++ app/src/main/java/amitApps/media/musicplayer/PlayerService.kt
@@ -1,5 +1,8 @@
 package amitApps.media.musicplayer
 
+import android.app.PendingIntent
+import android.content.Intent
+import android.widget.RemoteViews
 import java.beans.PropertyChangeEvent
 import java.beans.PropertyChangeListener
 import java.nio.channels.Channel
@@ -21,6 +24,20 @@ class PlayerService: android.app.Service(), PropertyChangeListener {
         const val ACTION_FIND_NEW_SONG = "action.FIND_NEW_SONG"
         const val ACTION_NOT_SONG_FOUND = "action.NOT_FOUND"
     }
+    // For showing notification
+    private lateinit var smallRemoteView: RemoteViews
+    private lateinit var largeRemoteViews: RemoteViews
+
+    //
+    private lateinit var intentPREVIOUS: PendingIntent
+    private lateinit var intentPLAY: PendingIntent
+    private lateinit var intentNEXT: PendingIntent
+    private lateinit var intentCANCEL: PendingIntent
+
+
+
+
+
 
 
 }
