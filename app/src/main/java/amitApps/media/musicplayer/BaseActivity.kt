32011ea88ff2a31bc970943f8ded6f32d768cfc4
amitYadavDev <amit8955461017@gmail.com>
2023-10-27 17:56:33 +0530

    interface.

--- app/src/main/java/amitApps/media/musicplayer/BaseActivity.kt
+++ app/src/main/java/amitApps/media/musicplayer/BaseActivity.kt
@@ -1,10 +1,23 @@
 package amitApps.media.musicplayer
 
+import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 abstract class BaseActivity<P : BasePresenter<*>> : AppCompatActivity(),
     BaseView {
 
     protected lateinit var presenter: P
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        presenter = createPresenter()
+    }
+
+    override fun showToast(msg: String) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+    }
+
+    protected abstract fun createPresenter(): P
 
 }
