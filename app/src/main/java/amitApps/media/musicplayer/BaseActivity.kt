f38894bb8bea11f94c7017c7dd7303f42cb0b2fa
amitYadavDev <amit8955461017@gmail.com>
2023-10-27 18:10:03 +0530

    baseview coded.

--- app/src/main/java/amitApps/media/musicplayer/BaseActivity.kt
+++ app/src/main/java/amitApps/media/musicplayer/BaseActivity.kt
@@ -1,8 +1,11 @@
 package amitApps.media.musicplayer
 
+import android.content.Context
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
 
 abstract class BaseActivity<P : BasePresenter<*>> : AppCompatActivity(),
     BaseView {
@@ -17,6 +20,12 @@ abstract class BaseActivity<P : BasePresenter<*>> : AppCompatActivity(),
     override fun showToast(msg: String) {
         Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
     }
+    override fun showSnackBar(v: View, msg: String) {
+        Snackbar.make(v, msg, Snackbar.LENGTH_SHORT).show()
+    }
+
+    override fun context(): Context = this
+
 
     protected abstract fun createPresenter(): P
 
