fd1b9258098a549accf76740dbab3eef5afa5410
amitYadavDev <amit8955461017@gmail.com>
2023-11-04 20:59:46 +0530

    animation

--- app/src/main/java/amitApps/media/musicplayer/FloatingAnimationView.kt
+++ app/src/main/java/amitApps/media/musicplayer/FloatingAnimationView.kt
@@ -1,12 +1,18 @@
 package amitApps.media.musicplayer
 
 import android.content.Context
+import android.graphics.Point
 import android.util.AttributeSet
+import java.util.Random
 
 class FloatingAnimationView constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
 ) : androidx.appcompat.widget.AppCompatImageView(context, attrs, defStyleAttr) {
+    lateinit var startPosition: Point
+    lateinit var endPosition: Point
+
+    private val random = Random()
 
 }
