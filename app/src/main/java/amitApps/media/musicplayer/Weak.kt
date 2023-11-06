44fc2317062b5def29224f92ec71af25f61344f1
amitYadavDev <amit8955461017@gmail.com>
2023-11-06 19:28:09 +0530

    weak class

--- app/src/main/java/amitApps/media/musicplayer/Weak.kt
+++ app/src/main/java/amitApps/media/musicplayer/Weak.kt
@@ -5,5 +5,9 @@ import java.lang.ref.WeakReference
 import kotlin.reflect.KProperty
 
 class Weak<T : Any>(initializer: () -> T?) {
+    var weakReference = WeakReference<T?>(initializer())
+
+    constructor() : this({ null })
+
 
 }
