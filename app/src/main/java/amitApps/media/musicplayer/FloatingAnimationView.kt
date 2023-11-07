7c6be0a72556123e3feeb5b6f8c4ac32d9885f5a
amitYadavDev <amit8955461017@gmail.com>
2023-11-07 18:25:00 +0530

    bazier curved implementation

--- app/src/main/java/amitApps/media/musicplayer/FloatingAnimationView.kt
+++ app/src/main/java/amitApps/media/musicplayer/FloatingAnimationView.kt
@@ -17,8 +17,23 @@ class FloatingAnimationView constructor(
     private val random = Random()
 
     fun startAnimation() {
+        val width = getScreenWidth()
+        val height = getScreenHeight()
 
+//        random.nextInt(width): This part generates a random integer
+//        between 0 (inclusive) and the value of the width variable (exclusive)
+        val endPointRandom =
+            Point(random.nextInt(width), endPosition.y)
 
+//        bezierTypeEvaluator is an instance of a BezierEvaluator class or
+//        function that uses random control points for defining Bezier curves.
+//        Bezier curves are often used in graphics and animation to create smooth and curved paths
+//        for objects or animations
+
+        val bezierTypeEvaluator = BezierEvaluator(
+            Point(random.nextInt(width), random.nextInt(height)),
+            Point(random.nextInt(width / 2), random.nextInt(height / 2))
+        )
     }
 
 
